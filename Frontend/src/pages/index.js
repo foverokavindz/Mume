af5e33b8dc3f5807ed4015d9319ce9b9004910fa
amareLD <80774058+amareLD@users.noreply.github.com>
2023-10-10 20:00:00 +0530

    make-fronend&backend-folder-strcture

---
+++ Frontend/src/pages/index.js
@@ -0,0 +1,17 @@
+import Home from './Home';
+import TopArtists from './TopArtists';
+import ArtistDetails from './ArtistDetails';
+import SongDetails from './SongDetails';
+import Search from './Search';
+import TopCharts from './TopCharts';
+import AroundYou from './AroundYou';
+
+export {
+  Home,
+  Search,
+  TopArtists,
+  ArtistDetails,
+  SongDetails,
+  TopCharts,
+  AroundYou,
+};
